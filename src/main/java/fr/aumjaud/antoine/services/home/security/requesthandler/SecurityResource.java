64754f15975a4c506f54eeea5b847dbaad82eb9c
Antoine Aumjaud <antoine_dev@aumjaud.fr>
2017-09-12 23:18:33 +0200

    :sparkles: manage synology surveillance station events

--- src/main/java/fr/aumjaud/antoine/services/home/security/requesthandler/SecurityResource.java
+++ src/main/java/fr/aumjaud/antoine/services/home/security/requesthandler/SecurityResource.java
@@ -1,5 +1,7 @@
 package fr.aumjaud.antoine.services.home.security.requesthandler;
 
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.aumjaud.antoine.services.home.security.service.SecurityService;
+import fr.aumjaud.antoine.services.common.security.WrongRequestException;
 
 @RestController
 public class SecurityResource {
@@ -27,5 +30,15 @@ public class SecurityResource {
     @RequestMapping(value = "/secure/event/{sensorName}")
     public String event(@PathVariable String sensorName) {
         return securityService.event(sensorName);
+    }
+
+    private final static Pattern CAMERA_MESG_PATTERN = Pattern.compile("\\W");
+    @RequestMapping(value = "/secure/event/camera")
+    public String eventCamera(@RequestParam String message) {
+        String[] items = CAMERA_MESG_PATTERN.split(message, 2);
+        if(items == null || items.length < 2) {
+            throw new WrongRequestException("wrong message", "event/camera: " + message + " doesn't match pattern");
+        }
+        return securityService.event("camera " + items[1]);
     }
 }
